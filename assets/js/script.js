3a3f1f3e335ee3700d705c0faafe3e3484bf8dcd
Adrien Miko <mikolajczykadrien@gmail.com>
2024-02-01 16:54:00 +0100

    ajout projet password-generator avec peu de style

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "!@#$%^&*_+-=~'\"`|/:;,.?";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
+
+const generatePassword = () => {
+  let data = [];
+  let password = "";
+
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (numbers.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des critères");
+    return;
+  }
+  for (i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+  }
+  passwordOutput.value = password;
+};
+
+generateButton.addEventListener("click", generatePassword);
