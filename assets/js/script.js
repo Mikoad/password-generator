616133029a32e8fa68edd15ed14fae60a7add3f6
amikolajczyk <amikolajczyk@SAT3P5CYT4.fayat.lan>
2025-07-09 10:59:52 +0200

    add copy to clipboard on click, to do : add modal for docs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const dataSymbols = "!@#$%^&*_+-=~'\"`|/:;,.?";
 const rangeValue = document.getElementById("password-length");
 const lengthDisplay = document.getElementById("length-display");
 const passwordOutput = document.getElementById("password-output");
+const copyMessage = document.getElementById("copy-message");
+// const helpModal = document.querySelector(".fa-question");
 lengthDisplay.textContent = rangeValue.value;
 const generatePassword = () => {
   let data = [];
@@ -28,3 +30,22 @@ rangeValue.addEventListener("input", () => {
   lengthDisplay.textContent = rangeValue.value;
 });
 generateButton.addEventListener("click", generatePassword);
+
+passwordOutput.addEventListener("click", () => {
+  const password = passwordOutput.value;
+  if (password) {
+    navigator.clipboard
+      .writeText(password)
+      .then(() => {
+        copyMessage.style.display = "inline";
+        passwordOutput.style.background = "#4a91e259";
+        setTimeout(() => {
+          copyMessage.style.display = "none";
+          passwordOutput.style.background = "none";
+        }, 1500);
+      })
+      .catch((err) => {
+        console.error("Erreur lors de la copie :", err);
+      });
+  }
+});
